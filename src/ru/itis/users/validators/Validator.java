faa55a989b1246e947abde777009eb75f88389cc
PythonGuySup <egorkachanov2006@gmail.com>
2024-11-05 21:34:14 +0300

    bug fix

--- src/ru/itis/users/validators/Validator.java
+++ src/ru/itis/users/validators/Validator.java
@@ -1,5 +1,5 @@
 package ru.itis.users.validators;
 
 public interface Validator<T> {
-    public boolean validate(T toValidate) throws NullPointerException;
+    boolean validate(T toValidate) throws NullPointerException;
 }
